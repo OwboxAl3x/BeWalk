cc041aaea72ee5e38df8c95b03759b4da481f1ee
Alejandro García <agarciavallecillo@gmail.com>
2018-06-16 00:11:56 +0200

    Proyecto creado

---
+++ BeWalk/BeWalk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BeWalk
+//
+//  Created by Alejandro Garcia Vallecillo on 16/6/18.
+//  Copyright © 2018 Alejandro Garcia Vallecillo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
